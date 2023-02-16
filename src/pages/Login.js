c0f1d6f897a7f7f2e0e547474a0e63c398c0878a
sonupall <sonu.bvminfotech@gmail.com>
2023-02-16 22:35:41 +0530

    create-login-page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,14 +4,18 @@ const Login = () =>{
     const [data , setData] = useState([]);
     const [successMsg, setSuccessMsg] = useState("");
     const [show, setShow] = useState(false);
+    const [user, setUser] = useState(false);
     const {
         register,
         handleSubmit,
         reset,
         formState: { errors },
     } = useForm();
+    const handleChange=(e)=>{
+        setUser({...user,[e.target.name]:e.target.value});
+    }
     const onSubmit = (data) => {
-        console.log(data);
+        setData([data]);
         setSuccessMsg("User registration is successful.");
         reset();
     };
@@ -35,10 +39,12 @@ const Login = () =>{
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                         </svg>
                         <input className="pl-2 outline-none border-none " type="text" name="" id="" placeholder="Email Address"
+                               onChange={(e)=>handleChange(e)}
                                {...register("email", {
                             required: "Email is required.",
                             pattern: {value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
                             message:  "Email is not valid."}
+
                         })} />
                     </div>
                         {errors.email && <div className="text-red-500 text-sm mt-1 mb-4 pl-4">{errors.email.message}</div>}
@@ -48,7 +54,9 @@ const Login = () =>{
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
                         </svg>
-                        <input className="pl-2 outline-none border-none" type={show ? "text": "password"} name="" id="" placeholder="Password"  {...register("password", {
+                        <input className="pl-2 outline-none border-none" type={show ? "text": "password"} name="" id="" placeholder="Password"
+                               onChange={(e)=>handleChange(e)}
+                               {...register("password", {
                             required: true,
                             validate: {
                                 checkLength: (value) => value.length >= 6,
