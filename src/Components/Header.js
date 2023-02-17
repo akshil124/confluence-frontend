5dec96b1d05e0546bbc9c36ae68dfe729f9b3d95
kartik <kartikborda.bvminfotech@gmail.com>
2023-02-17 16:30:42 +0530

    header-completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,17 @@
 import React, {useState} from "react";
 
 const Header=()=>{
-    const [open,setOpen]=useState(false)
+    const [open,setOpen]=useState(null)
+    const [isOpen,setIsOpen]=useState(null)
 
     const handleOpen=()=>{
-        setOpen(true)
+        setOpen(!open)
+    }
+    const handleIsOpen=()=>{
+        setIsOpen(!isOpen)
+    }
+    const handleClose=()=>{
+        setOpen(false)
     }
     const handleMenu=(e)=>{
         if (e==="profile"){
@@ -15,14 +22,15 @@ const Header=()=>{
             setOpen(false)
         }
     }
+
     return(
         <>
-            <nav className="bg-blue-800">
+            <nav className="bg-slate-50 shadow-lg ">
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="relative flex h-16 items-center justify-between">
                         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                            <button type="button"
-                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                            <button type="button" onClick={handleIsOpen}
+                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                                     aria-controls="mobile-menu" aria-expanded="false">
                                 <span className="sr-only">Open main menu</span>
                                 <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -48,35 +56,25 @@ const Header=()=>{
                             <div className="hidden sm:ml-6 sm:block">
                                 <div className="flex space-x-4">
                                     <a href="#"
-                                       className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                                       className="bg-blue-700 text-white px-3 py-2 rounded-md text-sm font-medium"
                                        aria-current="page">Dashboard</a>
 
                                     <a href="#"
-                                       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                                       className="text-gray-700 hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
 
                                     <a href="#"
-                                       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                                       className="text-gray-700 hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
                                     <a href="#"
-                                       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                                       className="text-gray-700 hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Sign-in</a>
                                 </div>
                             </div>
                         </div>
                         <div
                             className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                            <button type="button"
-                                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                                <span className="sr-only">View notifications</span>
-                                <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                     viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                          d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
-                                </svg>
-                            </button>
-
                             <div className="relative ml-3">
                                 <div>
-                                    <button type="button" onClick={handleOpen}
+                                    <button  type="button" onClick={handleOpen}
                                             className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                             id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                         <span className="sr-only">Open user menu</span>
@@ -86,8 +84,8 @@ const Header=()=>{
                                     </button>
                                 </div>
 
-                                {open ? <div
-                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                {open && (<div
+                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none pointer-events-auto"
                                     role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button"
                                     tabIndex="-1">
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200"  role="menuitem" onClick={()=>handleMenu('profile')}
@@ -96,27 +94,27 @@ const Header=()=>{
                                        tabIndex="-1" id="user-menu-item-1">Settings</a>
                                     <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-slate-200" role="menuitem" onClick={()=>handleMenu('sign-out')}
                                        tabIndex="-1" id="user-menu-item-2">Sign out</a>
-                                </div> : null}
+                                </div>)}
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="sm:hidden" id="mobile-menu">
+                {isOpen ? (<div className="sm:hidden" id="mobile-menu">
                     <div className="space-y-1 px-2 pt-2 pb-3">
-                        <a href="#" className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                        <a href="#" className="bg-blue-700 text-white block px-3 py-2 rounded-md text-base font-medium"
                            aria-current="page">Dashboard</a>
 
                         <a href="#"
-                           className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+                           className="text-gray-700 hover:bg-blue-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
 
                         <a href="#"
-                           className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+                           className="text-gray-700 hover:bg-blue-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
 
                         <a href="#"
-                           className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                           className="text-gray-7   00 hover:bg-blue-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sign-in</a>
                     </div>
-                </div>
+                </div>) : null}
             </nav>
         </>
     )
