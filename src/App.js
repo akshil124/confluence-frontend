db1a25d203bd06ab267fdfee2867dfbbba7b5ac4
sonupall <sonu.bvminfotech@gmail.com>
2023-02-17 16:59:05 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Login from "./pages/Login";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/signup';
 function App() {
-  return (
-    <BrowserRouter>
-          <Routes>
-              <Route path="/login" element={<Login />}/>
-              <Route path="/signup" element={<SignUp />}/>
-          </Routes>
-      </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<SignUp />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,22 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-const Login = () =>{
-    const [data , setData] = useState([]);
-    const [successMsg, setSuccessMsg] = useState("");
+const Login = () => {
+    const [data, setData] = useState([]);
+    const [successMsg, setSuccessMsg] = useState('');
     const [show, setShow] = useState(false);
     const [user, setUser] = useState(false);
     const {
         register,
         handleSubmit,
         reset,
-        formState: { errors },
+        formState: { errors }
     } = useForm();
-    const handleChange=(e)=>{
-        setUser({...user,[e.target.name]:e.target.value});
+    const handleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value });
     };
-    const onSubmit = (data) => {
-        setData([data]);
-        setSuccessMsg("User registration is successful.");
+    const onSubmit = (e) => {
+        setData([...data, user]);
+        setSuccessMsg('User registration is successful.');
         reset();
     };
     return (
@@ -39,45 +39,47 @@ const Login = () =>{
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                             </svg>
                             <input className="pl-2 outline-none border-none " type="text" name="" id="" placeholder="Email Address"
-                                   onChange={(e)=>handleChange(e)}
-                                   {...register("email", {
-                                       required: "Email is required.",
-                                       pattern: {value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-                                           message:  "Email is not valid."}
-                                   })} />
+                                onChange={(e) => handleChange(e)}
+                                {...register('email', {
+                                    required: 'Email is required.',
+                                    pattern: {
+                                        value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                                        message: 'Email is not valid.'
+                                    }
+                                })} />
                         </div>
                         {errors.email && <div className="text-red-500 text-sm mt-1 mb-4 pl-4">{errors.email.message}</div>}
                     </div>
                     <div>
                         <div className="flex items-center border-2 py-2 px-3 rounded-2xl">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                                <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
                             </svg>
-                            <input className="pl-2 outline-none border-none" type={show ? "text": "password"} name="" id="" placeholder="Password"
-                                   onChange={(e)=>handleChange(e)}
-                                   {...register("password", {
-                                   required: true,
-                                   validate: {
-                                       checkLength: (value) => value.length >= 6,
-                                       matchPattern: (value) =>
-                                           /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])/.test(
-                                               value
-                                           )
-                                   }
-                                   })}/>
-                            <label onClick={()=>{setShow(!show)}}
-                                   className="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label"
-                            >{show ? "hide" : "show"}</label>
+                            <input className="pl-2 outline-none border-none" type={show ? 'text' : 'password'} name="" id="" placeholder="Password"
+                                onChange={(e) => handleChange(e)}
+                                {...register('password', {
+                                    required: true,
+                                    validate: {
+                                        checkLength: (value) => value.length >= 6,
+                                        matchPattern: (value) =>
+                                            /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)(?=.*[!@#$*])/.test(
+                                                value
+                                            )
+                                    }
+                                })}/>
+                            <label onClick={() => { setShow(!show); }}
+                                className="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label"
+                            >{show ? 'hide' : 'show'}</label>
                         </div>
-                        {errors.password?.type === "required" && (
+                        {errors.password?.type === 'required' && (
                             <p className="text-red-500 text-sm mt-1 mb-4 pl-4">Password is required.</p>
                         )}
-                        {errors.password?.type === "checkLength" && (
+                        {errors.password?.type === 'checkLength' && (
                             <p className="text-red-500 text-sm mt-1 mb-4 pl-4">
                                 Password should be at-least 6 characters.
                             </p>
                         )}
-                        {errors.password?.type === "matchPattern" && (
+                        {errors.password?.type === 'matchPattern' && (
                             <p className="text-red-500 text-sm mt-1 mb-4 pl-4">
                                 Password should contain at least one uppercase letter,<br/> lowercase
                                 letter, digit, and special symbol.
@@ -90,5 +92,5 @@ const Login = () =>{
             </div>
         </div>
     );
-}
+};
 export default Login;
