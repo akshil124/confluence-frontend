5d5d5bf1eb48e9f5c96710db0dd20e00ef7b90a9
PC <user@gmail.com>
2023-02-21 10:47:47 +0530

    apolloprovider change file

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import React from 'react';
 import Login from './pages/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/signup';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-const client = new ApolloClient({
-    uri: 'http://localhost:3000/dev/graphql',
-    cache: new InMemoryCache()
-});
 function App () {
     return (
         <BrowserRouter>
-            <ApolloProvider client={client}>
-                <Routes>
-                    <Route path="/login" element={<Login />}/>
-                    <Route path="/signup" element={<SignUp />}/>
-                </Routes>
-            </ApolloProvider>
+            <Routes>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<SignUp />}/>
+            </Routes>
         </BrowserRouter>
     );
 }
