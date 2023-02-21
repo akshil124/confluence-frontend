66d415f182c2bf0a9204b23e422beb09b2957777
sonupall <sonu.bvminfotech@gmail.com>
2023-02-21 18:07:27 +0530

    login-completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Login from './pages/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/signup';
 import Plan from './Components/PricingPlan/Plan';
-// import KanbanBoard from './Components/KanbanBoard/index';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
